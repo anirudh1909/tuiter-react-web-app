a0c128316a7d06438993717fe9e9767dcc215944
Anirudh <anirudh.19090@gmail.com>
2023-06-13 16:14:33 -0400

    v8

--- src/tuiter/tuits/tuit-list.js
+++ src/tuiter/tuits/tuit-list.js
@@ -13,6 +13,11 @@ const TuitsList = () => {
 
   return (
     <ul className="list-group">
+      {loading && (
+        <li className="list-group-item">
+          Loading...
+        </li>
+      )}
 
       {tuits.map((tuit) => (
         <TuitItem key={tuit._id} tuit={tuit} />
